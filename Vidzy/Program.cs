72b12a2c455d65dd407a485a08217f6911a97e54
Agustin Prieto <agustinprieto.ar@gmail.com>
2019-08-20 23:33:01 -0300

    AddClassificationColumnToVideosTable

--- Vidzy/Program.cs
+++ Vidzy/Program.cs
@@ -13,6 +13,7 @@ namespace Vidzy
         public string Name { get; set; }
         public DateTime RealeaseDate { get; set; }
         public Genre VideoGenre { get; set; }
+        public Classification Classification { get; set; }
     }
 
     public class Genre
@@ -20,6 +21,13 @@ namespace Vidzy
         public byte Id { get; set; }
         public string Name { get; set; }
         public IList<Video> Videos { get; set; }
+    }
+
+    public enum Classification
+    {
+        Silver = 1,
+        Gold = 2,
+        Platinum = 3
     }
 
     public class VidzyContext : DbContext
